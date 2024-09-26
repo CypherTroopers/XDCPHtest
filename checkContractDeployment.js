fce4fce5b66d4bc221492ffd5df966962585e931
CypherTroopers <jin360life@gmail.com>
2024-09-26 18:38:09 +0700

    Update checkContractDeployment.js

--- checkContractDeployment.js
+++ checkContractDeployment.js
@@ -1,21 +1,21 @@
 const { ethers } = require('ethers');
 
-// Cypherium RPC URL
+
 const RPC_URL = 'http://218.185.241.160:8000';
 
-// プロバイダーを設定（Cypherium RPC）
+
 const provider = new ethers.providers.JsonRpcProvider(RPC_URL);
 
-// スマートコントラクトアドレス
+
 const contractAddress = '0x1E363768199697A07Af5B89028554D29c2BDfF84';
 
-// コントラクトがデプロイされているかを確認する関数
+
 async function checkContractDeployment() {
     try {
-        // 指定したアドレスからスマートコントラクトのコードを取得
+      
         const code = await provider.getCode(contractAddress);
 
-        // コードが存在するかチェック（'0x'はコードが存在しない場合）
+        
         if (code !== '0x') {
             console.log(`Contract is deployed at address: ${contractAddress}`);
         } else {
@@ -26,5 +26,5 @@ async function checkContractDeployment() {
     }
 }
 
-// 実行
+
 checkContractDeployment();
