ec546be836497b48310158c387945293f81d7f7b
CypherTroopers <jin360life@gmail.com>
2024-09-26 18:38:58 +0700

    Update TXcheck.js

--- TXcheck.js
+++ TXcheck.js
@@ -1,14 +1,14 @@
 const{  Web3 } = require('web3');
 
-// RPCノードのURLを指定 (CypheriumノードまたはEVM互換ノードのRPCエンドポイント)
+
 const web3 = new Web3(new Web3.providers.HttpProvider('http://218.185.241.160:8000'));
 
-// トランザクションハッシュを指定
+
 const transactionHash = '0x86ce04175400973c12fffd4b28e107f97e882521fd36d4498983c8d94670dd66';
 
 async function getTransactionReceipt() {
   try {
-    // トランザクションのレシートを取得
+    
     const receipt = await web3.eth.getTransactionReceipt(transactionHash);
     
     // トランザクションのレシートを出力
