d06d0a82d91504d13eb0cbb4c72944af3bfe31b5
CypherTroopers <jin360life@gmail.com>
2024-09-26 18:38:33 +0700

    Update check.js

--- check.js
+++ check.js
@@ -1,18 +1,18 @@
 const { ethers } = require('ethers');
 
-// Cypherium RPC URL
+
 const RPC_URL = 'http://218.185.241.160:8000';
 
-// 調べたいウォレットアドレス
+
 const address = '0x1E363768199697A07Af5B89028554D29c2BDfF84';
 
-// メイン処理
+
 async function getNativeBalance() {
-    // プロバイダーを設定
+  
     const provider = new ethers.providers.JsonRpcProvider(RPC_URL);
 
     try {
-        // ウォレットのネイティブ残高を取得
+      
         const balance = await provider.getBalance(address);
         console.log(`Native CPH balance of ${address}:`, ethers.utils.formatEther(balance), 'CPH');
     } catch (error) {
